d8e6a27096186b0be2680f43a1a58a41548e8240
marwan <saydmarwan841@gmail.com>
2026-01-16 19:05:48 +0300

    my_app

--- quiz_app/quiz_project/settings.py
+++ quiz_app/quiz_project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-romantic-quiz-app-secret-key-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
